75b12f134f52c87a5bb3269d97093e64f2293304
jonaslindell-th <Carljonaslindell@gmail.com>
2022-04-28 22:34:16 +0200

    chore: init WeatherForecastController

---
+++ Karlstad.WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using Karlstad.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Karlstad.WebApp.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        private readonly IWeatherForecastRepository _weatherForecastRepository;
+        public WeatherForecastController(IWeatherForecastRepository weatherForecastRepository)
+        {
+            _weatherForecastRepository = weatherForecastRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            WeatherForecast weatherForecast = await _weatherForecastRepository.GetKarlstadWeatherForecast();
+            return View();
+        }
+    }
+}
