20ac74bc30f28ec9fdf27c459ca39368c77476de
jonaslindell-th <Carljonaslindell@gmail.com>
2022-04-28 20:11:05 +0200

    feat: initial .net 5 mvc project

---
+++ Karlstad.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Karlstad.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
