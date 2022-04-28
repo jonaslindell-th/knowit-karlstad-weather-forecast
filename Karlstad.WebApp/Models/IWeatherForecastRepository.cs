539068f2155e0aedd13dce65ce2d376381924a63
jonaslindell-th <Carljonaslindell@gmail.com>
2022-04-28 22:33:04 +0200

    chore: init weatherforecastrepository

---
+++ Karlstad.WebApp/Models/IWeatherForecastRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Karlstad.WebApp.Models
+{
+    public interface IWeatherForecastRepository
+    {
+        Task<WeatherForecast> GetKarlstadWeatherForecast();
+    }
+}
