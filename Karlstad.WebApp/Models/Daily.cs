5e6bb5b6282c8247433b8b96c357e82f6b290f77
jonaslindell-th <Carljonaslindell@gmail.com>
2022-04-29 21:18:15 +0200

    chore: include wind speed

--- Karlstad.WebApp/Models/Daily.cs
+++ Karlstad.WebApp/Models/Daily.cs
@@ -5,6 +5,7 @@ namespace Karlstad.WebApp.Models
     public class Daily
     {
         public int Dt { get; set; }
+        public double Wind_Speed { get; set; }
         public Temperature Temp { get; set; }
         public IEnumerable<Weather> Weather { get; set; }
     }
