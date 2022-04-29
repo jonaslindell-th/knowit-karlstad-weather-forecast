ba47d31d681866e8f04041738a8264121c7cc2ef
jonaslindell-th <Carljonaslindell@gmail.com>
2022-04-29 19:17:10 +0200

    chore: adds weather description and icon

--- Karlstad.WebApp/Models/Daily.cs
+++ Karlstad.WebApp/Models/Daily.cs
@@ -1,8 +1,11 @@
-﻿namespace Karlstad.WebApp.Models
+﻿using System.Collections.Generic;
+
+namespace Karlstad.WebApp.Models
 {
     public class Daily
     {
         public int Dt { get; set; }
         public Temperature Temp { get; set; }
+        public IEnumerable<Weather> Weather { get; set; }
     }
 }
---
+++ Karlstad.WebApp/Models/Weather.cs
@@ -0,0 +1,8 @@
+﻿namespace Karlstad.WebApp.Models
+{
+    public class Weather
+    {
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
